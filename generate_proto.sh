a893a85d0c97e20e5f44976cc0bd3f6e1fb0c93c
Jacek Ciesielski <jot.ciesielski@gmail.com>
2022-10-15 18:51:35 +0200

    Add js proto definitions generation to the proto generator script
    Adding the npm tool for js proto definition generation.

--- generate_proto.sh
+++ generate_proto.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-protoc --go_out=grpc-server/ --go-grpc_out=grpc-server/ scrape.proto
+protoc --go_out=grpc-server/ --go-grpc_out=grpc-server/ --grpc-web_out=import_style=commonjs,mode=grpcweb:web-client/ scrape.proto
+./web-client/node_modules/.bin/grpc_tools_node_protoc --js_out=import_style=commonjs,binary:web-client/ --grpc_out=grpc_js:web-client/ scrape.proto
